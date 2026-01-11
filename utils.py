50fe2947014ce3ec7070bb10d71422cf07984c51
Fatikh Afan Kurniawan <omtanpanama@gmail.com>
2026-01-11 20:26:45 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,31 +1,24 @@
 import tensorflow as tf
-import os
+import numpy as np
 import gdown
+import os
 import streamlit as st
 
 @st.cache_resource
 def load_model_cloud():
-    # ID File dari Google Drive Anda
     file_id = '1i6RBVKi9BmSY_jvlypbMWOwvoO5Dyn1t'
     url = f'https://drive.google.com/uc?id={file_id}'
     output = 'model_ready_petani.h5'
-    
-    # Cek apakah file sudah ada, jika belum download
     if not os.path.exists(output):
-        try:
-            with st.spinner("✨ Menghubungkan ke Otak AI Petani_Abies..."):
-                gdown.download(url, output, quiet=False)
-        except Exception as e:
-            st.error(f"Gagal mengunduh model: {e}")
-            return None
-            
-    # Load model tanpa kompilasi agar lebih cepat
+        with st.spinner("✨ Menghubungkan ke Otak AI..."):
+            gdown.download(url, output, quiet=False)
     return tf.keras.models.load_model(output, compile=False)
 
 def preprocess_image(image):
-    """Fungsi untuk memproses gambar agar sesuai input model ResNet50"""
+    # Resize ke 224x224 (Standar ResNet50)
     img = image.resize((224, 224))
     x = tf.keras.preprocessing.image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
+    # Sangat penting: gunakan preprocess_input bawaan ResNet50
     x = tf.keras.applications.resnet50.preprocess_input(x)
     return x
